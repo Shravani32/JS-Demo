cfd179807acf99b78bcc77ee19ab0217a7a7665e
Shravani32 <131517563+Shravani32@users.noreply.github.com>
2024-06-17 12:43:39 +0000

    Date time done

---
+++ 01_basics/_date.js
@@ -0,0 +1,22 @@
+// let myDate=new Date()
+// console.log(myDate.toDateString())
+// // console.log(myDate.toJSON())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate)
+
+let myCreateDate = new Date(2024,5,17,5,6)
+// console.log(myCreateDate.toLocaleString())
+
+// let newDate=new Date("2023-01-14")
+// console.log(newDate.toLocaleString());
+
+// let myTimestamp = Date.now();
+// console.log(myTimestamp);
+// console.log(myCreateDate.getTime())
+
+// console.log(Date.now() / 1000);
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate=new Date();
+console.log(newDate)
+console.log(newDate.getMonth());
---
+++ 01_basics/_strings.js
@@ -0,0 +1,20 @@
+const name="hitesh"
+const repoCount=50
+
+// console.log(name + reCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName=new String('hitesh')
+// console.log(gameName.__proto__);
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.toLowerCase())
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('t'))
+
+// console.log(gameName.substring(0,4))  //negative indexes are not allowed
+// console.log(gameName.slice(-5,4)) //negative indexes are allowed
+
+console.log(gameName.slice(0,5))
+
