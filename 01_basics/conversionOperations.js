3caec552e985d56a277bebf5833a34680ef5ec54
Shravani32 <131517563+Shravani32@users.noreply.github.com>
2024-06-17 06:23:55 +0000

    done

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,23 @@
+// let score=true
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// let isLoggedin=""
+
+// let booleanIsLoggedIn = Boolean(isLoggedin)
+// console.log(booleanIsLoggedIn)
+
+//1-> true; 0->false
+//""=>false
+//"shravani"=>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+
+console.log(typeof stringNumber)
+console.log(stringNumber)
