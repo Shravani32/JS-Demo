44b3d8e343a6dfa7ab9ea3cab1b11a5794ebc473
Shravani32 <131517563+Shravani32@users.noreply.github.com>
2024-06-17 11:34:18 +0000

    stack heap done

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -13,22 +13,57 @@
 
 //Array , Objects , Functions
 
-const heros=["Shaktiman","Nagarjun","Doga"]
+// const heros=["Shaktiman","Nagarjun","Doga"]
 
-let myObj={
-    name:"Shravani",
-    age:20,    
-}
+// let myObj={
+//     name:"Shravani",
+//     age:20,    
+// }
 
 // console.log(myObj)
 
-const myFunc=function (){
-     console.log("Hello World!!");
-}
+// const myFunc=function (){
+//      console.log("Hello World!!");
+// }
 
 // const func=myFunc()
 // console.log(myFunc())
 
 // console.log(typeof myFunc);
 // console.log(typeof myObj)
-console.log(typeof heros)
+// console.log(typeof heros)
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive) copy, Heap (Non-primitive) reference
+
+// let myName="shravanijoshi.com" //primitive goes in stack
+// let anotherName=myName
+// console.log(anotherName);
+
+let obj={
+    name:"ssj",
+    age:22
+}                //non-primitive goes in heap
+
+
+let user={
+    email:"user@gmail.com",
+    username:"ggg",
+    password:"user@123"
+}
+
+let myObj=user;
+// console.log(myObj);
+
+myObj.email="ssj@gmail.com"
+
+// myObj={
+//     name:"ssj",
+//     age:22
+// }
+
+console.log(user.email)
+console.log(myObj.email)
+
