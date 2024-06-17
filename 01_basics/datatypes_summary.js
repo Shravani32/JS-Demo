1229a37e483099391e05b559cbec7ec6345b96ef
Shravani32 <131517563+Shravani32@users.noreply.github.com>
2024-06-17 11:16:59 +0000

    data  types summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,34 @@
+//primitive data types:
+
+//String ,number,Boolean,null,undefined,Symbol,BigInt
+
+// const iD = Symbol('123');
+// const iD2=Symbol('123')
+// console.log(iD==iD2)
+
+// const bigNum=843756169417494748n
+// console.log(bigNum)
+
+//Reference data types(non-primitive)
+
+//Array , Objects , Functions
+
+const heros=["Shaktiman","Nagarjun","Doga"]
+
+let myObj={
+    name:"Shravani",
+    age:20,    
+}
+
+// console.log(myObj)
+
+const myFunc=function (){
+     console.log("Hello World!!");
+}
+
+// const func=myFunc()
+// console.log(myFunc())
+
+// console.log(typeof myFunc);
+// console.log(typeof myObj)
+console.log(typeof heros)
