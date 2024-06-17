61e9a9839ea52f08056840520a5df085293eaa4a
Shravani32 <131517563+Shravani32@users.noreply.github.com>
2024-06-17 05:53:23 +0000

    done

---
+++ 01_basics/_variables.js
@@ -0,0 +1,16 @@
+const accountId=1443
+let accountEmail="shravani@gmail.com"
+var password="23as"
+accountCity='Jaipur'
+let accountState=null;
+// accountId=43;
+accountEmail="245"
+password="637678"
+
+console.log(accountEmail);
+
+/*
+  prefer not to use var because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountCity,accountState])
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer 
+
+// alert(3+3) //we are using node js not browser
+
+console.log(3+3)
+//code readability should be high
+console.log("hitech")
+
+let name="shravani"
+let age=18
+let isLoggedin=false
+
+
+//number => 2 to power 53
+//bigint 
+//string=""
+//boolean=>true/false
+//null => standalone value
+//undefined => not intialized with any value
+//symbol=>unique
+
+//object
+
+console.log(typeof null)  //null is object
+console.log(typeof undefined)
